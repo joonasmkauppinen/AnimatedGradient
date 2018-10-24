e72dd5deaca141587a9148c8952f4d9f9b0b558f
Joonas Kauppinen <joonasm.kauppinen@gmail.com>
2018-10-24 10:42:05 +0300

    Initial commit

---
+++ app/src/main/java/com/example/animatedgradient/MainActivity.kt
@@ -0,0 +1,23 @@
+package com.example.animatedgradient
+
+import android.graphics.drawable.AnimationDrawable
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        window.decorView.systemUiVisibility = View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+
+        val animDrawable = root_layout.background as AnimationDrawable
+        animDrawable.setEnterFadeDuration(10)
+        animDrawable.setExitFadeDuration(5000)
+        animDrawable.start()
+
+    }
+}
